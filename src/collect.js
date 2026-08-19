ff9b5232d09d548c31af77d63437f940906b278a
mathiiiiiis <mathis@mathiiis.de>
2026-08-19 02:39:01 +0200

    feat(collect): read a pose back off the rig

---
+++ src/collect.js
@@ -0,0 +1,30 @@
+import { angleBetween, DEG, normalize } from "./quat";
+import { toWorld, toAxisAngle } from "./space";
+
+//ignore negligible movement
+const MOVED_DEG = 0.01;
+const PRECISION = 5;
+
+const round = (n) => Number(n.toFixed(PRECISION));
+
+export function collectPose(rig, getQuaternion) {
+  const rotation = {};
+
+  for (const name of rig.poseBones) {
+    const bone = rig.bones[name];
+    const q = getQuaternion(name);
+    if (!q) continue;
+    if (angleBetween(q, bone.rest.rotation) * DEG < MOVED_DEG) continue;
+
+    rotation[name] = normalize(q).map(round);
+  }
+
+  return { rotation };
+}
+
+export function describePose(rig, pose) {
+  return Object.entries(pose.rotation).map(([name, q]) => {
+    const { axis, degrees } = toAxisAngle(toWorld(rig.bones[name], q));
+    return { name, degrees, axis };
+  });
+}
